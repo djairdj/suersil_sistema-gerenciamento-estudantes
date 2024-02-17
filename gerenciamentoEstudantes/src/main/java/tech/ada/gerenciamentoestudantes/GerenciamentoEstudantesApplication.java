bd1fc1e66599b17c1c14491667d1b81973af14c0
Djair Alves <djair.dj@gmail.com>
2024-02-17 13:50:44 -0300

    Formatação

--- gerenciamentoEstudantes/src/main/java/tech/ada/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
+++ gerenciamentoEstudantes/src/main/java/tech/ada/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerenciamentoEstudantesApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(GerenciamentoEstudantesApplication.class, args);
   }
